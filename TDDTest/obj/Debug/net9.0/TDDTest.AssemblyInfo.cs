0992ae7eed956b7ae28179781925611882b3d3e8
Anomaly User <i@example.com>
2025-10-29 21:39:42 +0300

    Красная фаза пятого цикла

--- TDDTest/obj/Debug/net9.0/TDDTest.AssemblyInfo.cs
+++ TDDTest/obj/Debug/net9.0/TDDTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TDDTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4e9dbe7818ba985ded1049e16495c7e5534ed56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9191c3a789370e1b4c95be4c6c18fc8b74bd2faa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TDDTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TDDTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
